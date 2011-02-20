97b7916503e9677c54b37057e9bd04dae63084ee
Michael Cramer <cramer@webkist.com>
2011-02-20 09:08:46 -0500

    Initial checkin of LightMeter source.
    Only tested on HTC Hero so far. Very early.

---
+++ src/com/webkist/android/lightmeter/LightMeter.java
@@ -0,0 +1,40 @@
+package com.webkist.android.lightmeter;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+
+/**
+ * An example of tabs that uses labels (
+ * {@link TabSpec#setIndicator(CharSequence)}) for its indicators and views by
+ * id from a layout file ({@link TabSpec#setContent(int)}).
+ */
+public class LightMeter extends TabActivity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		Resources res = getResources(); // Resource object to get Drawables
+		TabHost tabHost = getTabHost(); // The activity TabHost
+		TabHost.TabSpec spec; // Resusable TabSpec for each tab
+		Intent intent; // Reusable Intent for each tab
+
+		// Create an Intent to launch an Activity for the tab (to be reused)
+		intent = new Intent().setClass(this, AvActivity.class);
+		
+		spec = tabHost.newTabSpec("Av").setIndicator("Aperture", res.getDrawable(R.drawable.ic_tab_av)).setContent(intent);
+		tabHost.addTab(spec);
+		spec = tabHost.newTabSpec("Tv").setIndicator("Shutter", res.getDrawable(R.drawable.ic_tab_tv)).setContent(intent);
+		tabHost.addTab(spec);
+		spec = tabHost.newTabSpec("P").setIndicator("Program", res.getDrawable(R.drawable.ic_tab_p)).setContent(intent);
+		tabHost.addTab(spec);
+		spec = tabHost.newTabSpec("M").setIndicator("Manual", res.getDrawable(R.drawable.ic_tab_m)).setContent(intent);
+		tabHost.addTab(spec);
+
+		tabHost.setCurrentTab(2);
+	}
+}
---
+++ src/com/webkist/android/lightmeter/TvActivity.java
@@ -0,0 +1,13 @@
+package com.webkist.android.lightmeter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TvActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tv);
+    }
+}
